c56d5ed573c951daffed1994aea993b7fc7719b8
gag1005 <gag1005@gmail.com>
2022-11-17 21:04:58 +0100

    [Frontend] Se empieza a hacer la pagina de discusiones
    Co-authored-by: glp1002 <glp1002@alu.ubu.es>
    Co-authored-by: gbd1004 <gbd1004@alu.ubu.es>
    Co-authored-by: sdg1002 <sdg1002@alu.ubu.es>
    Co-authored-by: gag1005 <gag1005@alu.ubu.es>

--- components/dms2223frontend/dms2223frontend/presentation/web/discussionendpoints.py
+++ components/dms2223frontend/dms2223frontend/presentation/web/discussionendpoints.py
@@ -1,131 +1,67 @@
 """ DiscussionEndpoints class module.
-
 """
 
-
-
 from typing import Text, Union
-
 from flask import redirect, url_for, session, render_template, request
-
 from werkzeug.wrappers import Response
-
 from dms2223common.data import Role
-
 from dms2223frontend.data.rest.authservice import AuthService
-
+from dms2223common.data.rest import questionsdb
 from .webauth import WebAuth
 
 
 
 
-
-
-
-
-
 class DiscussionEndpoints():
-
     """ Monostate class responsible of handling the discussion web endpoint requests.
-
     """
-
     @staticmethod
-
     def get_discussion(auth_service: AuthService) -> Union[Response, Text]:
-
         """ Handles the GET requests to the discussion root endpoint.
 
-
-
         Args:
-
             - auth_service (AuthService): The authentication service.
 
-
-
         Returns:
-
             - Union[Response,Text]: The generated response to the request.
-
         """
-
         if not WebAuth.test_token(auth_service):
-
             return redirect(url_for('get_login'))
-
         if Role.DISCUSSION.name not in session['roles']:
-
             return redirect(url_for('get_home'))
-
         name = session['user']
-
-        return render_template('discussion.html', name=name, roles=session['roles'])
-
+        return render_template('discussion.html', name=name, roles=session['roles'], questions=questionsdb.QuestionsDB().get_questions()[0]['questions'])
     
-
     @staticmethod
-
     def new_discussion(auth_service: AuthService) -> Union[Response, Text]:
-
         """ Handles the POST requests to discussion root endpoint.
 
-
-
         Args:
-
             - auth_service (AuthService): The authentication service.
 
-
-
         Returns:
-
             - Union[Response,Text]: The generated response to the request.
-
         """
-
         if not WebAuth.test_token(auth_service):
-
             return redirect(url_for('get_login'))
-
         if Role.DISCUSSION.name not in session['roles']:
-
             return redirect(url_for('get_home'))
-
         name = session['user']
-
         
-
         # Obtenemos los nuevos datos introducidos
-
         did = request.form.get('did')
-
         content = request.form.get('content')
-
         
-
         return render_template('discussion.html', name=name, roles=session['roles'],
-
         	#Añadir el resto de la estructura que metamos en la base de datos
-
         	did=int(did), content=content)
-
         
-
         # NOTA: ¿Añadir un redirect a /discussions?
-
         
 
 
-
-
-
         
-
         
-
         
-
         
-
         
