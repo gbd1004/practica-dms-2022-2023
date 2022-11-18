281a1a37d7f57d7c6707ecf63b1469e28819d805
Gonzalo <gbd1004@alu.ubu.es>
2022-11-18 23:40:37 +0100

    Añadido auth a backend (sigue sin ir)

--- components/dms2223backend/dms2223backend/presentation/security.py
+++ components/dms2223backend/dms2223backend/presentation/security.py
@@ -6,6 +6,24 @@ from authlib.jose import JsonWebSignature  # type: ignore
 from connexion.exceptions import Unauthorized  # type: ignore
 from dms2223backend.data.config.backendconfiguration import BackendConfiguration
 
+def verify_api_key(token: str) -> Dict:
+    """Callback testing the received API key.
+
+    Args:
+        - token (str): The received API key.
+
+    Raises:
+        - Unauthorized: When the given API key is not valid.
+
+    Returns:
+        - Dict: Information retrieved from the key to be passed to the endpoints.
+    """
+    with current_app.app_context():
+        cfg: BackendConfiguration = current_app.cfg
+        if not token in cfg.get_authorized_api_keys():
+            raise Unauthorized('Invalid API key')
+    return {}
+
 def verify_token(token: str) -> Dict:
     """Callback testing a JWS user token.
 
